912b1b3ec9eb7c453ad068257b56394903aebaad
Franciszek Adamski <adams.franciszek@wp.pl>
2026-07-30 19:06:09 +0200

    readd from x server

--- display_image.py
+++ display_image.py
@@ -6,18 +6,22 @@ import argparse
 import logging
 import time
 from PIL import Image
+import mss
 import traceback
 
 import epd7in5_V2
 from pdf_reader import LazyPDFReader 
 
+
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
 
 def main():
     parser = argparse.ArgumentParser(description="Display an image on a Waveshare 7.5inch e-Paper V2 screen.")
     parser.add_argument("image_path", help="Path to the image file (BMP, PNG, JPG, etc.)")
     parser.add_argument("--clear", action="store_true", help="Clear the screen before displaying the image")
-    parser.add_argument("--verbose", help="Logging for debugging")
+    parser.add_argument("--verbose", action="store_true", help="Logging for debugging")
+    parser.add_argument("--capture", action="store_true", help="Whether to capture image from running xserver session")
     args = parser.parse_args()
 
     if not os.path.exists(args.image_path):
@@ -36,8 +40,13 @@ def main():
         
         if args.verbose:
             logging.info(f"Processing image: {args.image_path}")
-        user_image = Image.open(args.image_path)
+        if args.capture:
+            user_image = capture_x11_frame()
+        else:
+            user_image = Image.open(args.image_path)
  
+        if args.verbose:
+            logging.info(f"Image size: {user_image.size}")
         bg_image = process_image(epd, args, user_image)
 
         # Push to display
@@ -62,11 +71,12 @@ def main():
         logging.error(f"An unexpected error occurred: {e}")
         traceback.print_exc()
 
+
 def process_image(epd, args, user_image):
         
     # Resize image to match screen dimensions (800x480 for 7.5" V2) while maintaining aspect ratio
     if args.verbose:
-        logging.info(f"Screen dimensions: {epd.width}x{epd.height}")
+        logging.info(f'Screen dimensions: {epd.width}x{epd.height}')
         
     # Create a blank white background matching exact screen dimensions
     bg_image = Image.new('1', (epd.width, epd.height), 255) 
@@ -83,6 +93,17 @@ def process_image(epd, args, user_image):
     bg_image.paste(user_image, (paste_x, paste_y))
 
     return bg_image
+
+
+def capture_x11_frame(display_name: str = ':0'):
+    os.environ['DISPLAY'] = display_name
+    if 'XAUTHORITY' not in os.environ:
+        os.environ['XAUTHORITY'] = f'{os.environ["HOME"]}/.Xauthority' 
+    with mss.mss() as screen:
+        monitor = screen.monitors[1]
+        image_bytes = screen.grab(monitor)
+        return Image.frombytes('RGB', image_bytes.size, image_bytes.bgra, 'raw', 'BGRX')
+
 
 
 if __name__ == '__main__':
