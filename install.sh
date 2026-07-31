c2083c0c1481a39f8a2c4ce54e2304897d278f2d
Franciszek Adamski <adams.franciszek@wp.pl>
2026-07-31 23:09:03 +0200

    update installation stuff

---
+++ install.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/bash
+
+
+
+echo "Installing xserver-xorg-video-dummy"
+
+sudo apt update
+sudo apt install xserver-xorg-video-dummy
+
+echo "Copying the dummy screen file"
+sudo cp dummy.conf /etc/X11/xorg.conf.d/20-dummy.conf
+
+echo "Installing einkrpi"
+
+if [ ! -d $HOME/.local/share]; then
+    echo "Creating $HOME/.local/share directory"
+    mkdir -p $HOME/.local/share
+fi
+
+if [ ! -d $HOME/.local/share/einkrpi ]; then
+    echo "Cloning einkrpi repository to $HOME/.local/share/"
+    git clone https://github.com/franciszekadamski/einkrpi.git
+fi
+
+cd $HOME/.local/share/einkrip
+git pull
+
+echo "Attempting to stop, disable and remove einkrpi.service for systemd"
+sudo systemctl stop einkrpi.service
+sudo systemctl disable einkrpi.service
+sudo rm /etc/systemd/einkrpi.service
+
+echo "Installing einkrpi.service"
+envsubst < $PWD/einkrpi.service.template | sudo tee /etc/systemd/system/einkrpi.service > /dev/null
+
+sudo systemctl deamon-reexec
+sudo systemctl deamon-reload
+sudo systemctl enable einkrpi.service
+sudo systemctl start einkrpi.service
+sudo systemctl status einkrpi.service
+
+echo "Finished installation of einkrpi"
+echo "The repository is located in $HOME/.local/share/einkrip directory"
+echo "Additionally, /etc/systemd/system/einkrpi.service file has been created"
+echo "Fake screen file created is /etc/X11/xorg.conf.d/20-dummy.conf"
