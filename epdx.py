cc84ad766f01c161a82ff0161dd70282cdbee4bb
Franciszek Adamski <adams.franciszek@wp.pl>
2026-08-01 21:47:16 +0200

    add partial refreshing

--- epdx.py
+++ epdx.py
@@ -42,20 +42,35 @@ class EPDX:
         self.process_image()
         similarity = self.phash_similarity()  
 
-        if 0.7 <= similarity < 1.0:
-            self.epd.init_fast()
-            self.epd.display(self.epd.getbuffer(self.image))
-            # self.epd.sleep()
-            self.previous_image = copy.copy(self.image)
-            # time.sleep(0.1)
-        elif similarity < 0.7:
-            self.epd.init()
-            self.epd.display(self.epd.getbuffer(self.image))
-            self.epd.sleep()
-            self.previous_image = copy.copy(self.image)
-            # time.sleep(0.1)
+        if 0.6 <= similarity < 1.0:
+            self.display_image_partial()
+        elif similarity < 0.6:
+            self.display_image()
         else:
             return
+
+    def display_image(self):
+        self.epd.init()
+        self.epd.display(self.epd.getbuffer(self.image))
+        self.epd.sleep()
+        self.previous_image = copy.copy(self.image)
+
+
+    def display_image_fast(self):
+        self.epd.init_fast()
+        self.epd.display(self.epd.getbuffer(self.image))
+        self.previous_image = copy.copy(self.image)
+
+
+    def display_image_partial(self):
+        self.epd.init_part()
+        self.epd.display_Partial(self.epd.getbuffer(self.image), 0, 0, self.epd.width, self.epd.height)
+        self.previous_image = copy.copy(self.image)
+
+
+    def get_differences_bounding_box_and_region(self):
+        difference_mask = ImageChops(self.previous_image, self.image)
+        return difference_mask.getbbox(), self.image.crop(bounding_box)
 
 
     def capture_x11_frame(self):
@@ -72,7 +87,7 @@ class EPDX:
         paste_x = (self.epd.width - self.image.size[0]) // 2
         paste_y = (self.epd.height - self.image.size[1]) // 2
         self.image = self.image.convert('L')
-        self.image = self.image.filter(ImageFilter.UnsharpMask(radius=100, percent=1500, threshold=3))
+        self.image = self.image.filter(ImageFilter.UnsharpMask(radius=150, percent=1500, threshold=3))
         self.image = self.image.convert('1') # Floyd-Steinberg dithering
         bg_image.paste(self.image, (paste_x, paste_y))
         self.image = bg_image
@@ -84,7 +99,6 @@ class EPDX:
         hamming_distance = hash1 - hash2
         max_bits = len(hash1.hash) ** 2
         return 1.0 - (hamming_distance / max_bits)
-
 
 
 if __name__ == '__main__':
